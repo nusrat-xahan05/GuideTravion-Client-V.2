3f25ae50c01bd1504507fc8d73c268da43cd819e
Nusrat Jahan <nusrat.jahan.az@gmail.com>
2026-01-18 21:09:16 +0600

    Updated favicon & Loading Animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,35 +1,21 @@
-import Image from "next/image";
-import logoImg from '../assets/images/logo.png'
+import Logo from "@/assets/icons/Logo";
 
 export default function Loading() {
     return (
-        <div className="flex min-h-[70vh] items-center justify-center bg-[#07102A]">
+        <div className="flex min-h-[70vh] items-center justify-center bg-[rgba(255,255,255,0.6)] font-body">
             <div className="flex flex-col items-center gap-3 text-center">
 
-                {/* Logo / Travel Indicator */}
-                <div className="flex flex-col gap-2 justify-center items-center">
-                    <div className="relative flex h-10 w-10 items-center justify-center">
-                        <Image
-                            src={logoImg}
-                            alt="GuideTravion"
-                            fill
-                            className="object-cover"
-                        />
-                    </div>
-
-                    <span className="text-white text-2xl font-semibold">
-                        GuideTravion
-                    </span>
-                </div>
+                {/* Logo */}
+                <Logo></Logo>
 
                 {/* Progress Bar */}
-                <div className="h-1 w-48 overflow-hidden rounded-full bg-white/10">
-                    <div className="h-full w-1/2 animate-loading-bar rounded-full bg-primary" />
+                <div className="h-1 w-56 overflow-hidden rounded-full bg-primary">
+                    <div className="h-full w-1/2 animate-loading-bar rounded-full bg-secondary" />
                 </div>
 
                 {/* Loading Text */}
                 <div className="mt-2.5">
-                    <p className="text-lg font-medium text-white">
+                    <p className="text-base font-medium text-[rgba(0,43,17,0.7)]">
                         Preparing your journey
                     </p>
                 </div>
